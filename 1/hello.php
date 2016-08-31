c7341b254053591873f6cbc9f0127f311c46c079
Des Dulianto <desdulianto@waruna-group.co.id>
2016-08-31 11:22:57 +0700

    Tambah README.md and source code pertemuan 1

---
+++ 1/hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello PHP</title>
+	<style>
+	<?php echo "p { color: red; }"; ?>
+	</style>
+</head>
+<body>
+<?php echo "<p>Hello World from PHP!</p>"; ?>
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # web2uph2016
+
+Source Code contoh pada perkuliahan Pemrograman Web 2 UPH Medan 2016 kelas sore
