8c0d25ecd5577acf454af014f4e407e1798e89d2
Des Dulianto <desdulianto@waruna-group.co.id>
2016-09-14 15:54:55 +0700

    contoh loop assosiatif dipecah menjadi 3 bentuk

--- 3/arrayassosiatif.php
+++ 3/arrayassosiatif.php
@@ -5,26 +5,51 @@
 </head>
 <body>
 <?php
-$kontak = array(array("nama"=>"Budi", "telp"=>"1234", "alamat"=>"def"),
-				array("nama"=>"Susi", "telp"=>"5678", "alamat"=>"jki"));
+$kontak = array(array("nama"=>"Budi", "telp"=>"1234", "alamat"=>"Jl. Asia"),
+				array("nama"=>"Susi", "telp"=>"5678", "alamat"=>"Jl. Thamrin"));
 ?>
 	<table>
-		<tr><th>Nama</th><th>Telp</th></tr>
+		<tr><th>Nama</th><th>Telp</th><th>Alamat</th></tr>
 <?php
-		$kolom = ["nama", "telp"];
 		for ($i = 0; $i < count($kontak); $i++) {
 			echo "<tr>";
-			//foreach ($kontak[$i] as $key => $value) {
-			//foreach ($kontak[$i] as $value) {
-			foreach ($kolom as $k) {
+            foreach ($kontak[$i] as $value) {
 				echo "<td>";
-				//echo $key . ": " . $value;
-				echo $kontak[$i][$k];	
+                echo $value;
 				echo "</td>";
 			}
 			echo "</tr>";
 		}
 ?>
 	</table>
+<?php
+    echo "<h1>Looping dengan key=>value</h1>";
+
+    echo "<ul>";
+    foreach ($kontak as $k) {
+        echo "<li>";
+        echo "<address>";
+        foreach ($k as $key => $value) {
+            echo "<b>$key</b>: $value<br>";
+        }
+        echo "</address>";
+        echo "</li>";
+    }
+    echo "</ul>";
+
+    echo "<h1>Looping untuk menampilkan data nama dan telp saja</h1>";
+    $keys = array("nama", "telp");
+    echo "<ul>";
+    foreach ($kontak as $k) {
+        echo "<li>";
+        echo "<address>";
+        foreach ($keys as $i) {
+            echo "<b>$i</b>: ". $k[$i] . "<br>";
+        }
+        echo "</address>";
+        echo "</li>";
+    }
+    echo "</ul>";
+?>
 </body>
 </html>
